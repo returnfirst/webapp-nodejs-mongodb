a1713c65db88f8b10249316851f9a6dcb8a42822
returnfirst <returnfirst@gmail.com>
2018-03-22 10:31:30 +0900

    module system에 대한 예제

---
+++ WebDevelopmentwithMongoDBandNod/calculator.js
@@ -0,0 +1,8 @@
+const calc = require('./superCalc');
+
+let numOne = 15;
+let numTwo = 20;
+
+let result = calc.add(numOne, numTwo);
+
+console.log(result);
---
+++ WebDevelopmentwithMongoDBandNod/superCalc.js
@@ -0,0 +1,17 @@
+// const add = function (a, b) {
+//     return a + b;
+// };
+
+// const sub = function (a, b) {
+//     return a - b;
+// };
+
+// module.exports.add = add;
+// module.exports.sub = sub;
+
+const calc = {
+    add: function(a, b) { return a + b },
+    sub: function(a, b) { return a - b },
+};
+
+module.exports = calc;
